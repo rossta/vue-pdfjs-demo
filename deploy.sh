f38298bc3cc9a72569e5ae2a276074320971341b
Ross Kaffenberger <rosskaff@gmail.com>
2018-06-11 07:56:35 -0400

    Deploy script in progress

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+CWD=`pwd`
+BRANCH=gh-pages
+
+# Clone Pages repository
+cd /tmp
+git clone $CIRCLE_REPOSITORY_URL dist
+
+cd dist && git checkout -b $BRANCH origin/$BRANCH
+
+# Trigger build
+cd $CWD
+yarn build
+
+# Push newly built repository
+cp -r $CWD/dist/* /tmp/dist
+
+cd /tmp/dist
+
+git config --global user.email "ross@rossta.net"
+git config --global user.name "Ross Kaffenberger"
+
+git add .
+
+$HEAD = `git log --pretty="%h" -n1`
+git commit -m "Site updated to $HEAD"
+
+git push -f origin
